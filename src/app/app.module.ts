ce7bc8fba1e7732cf2b92774e7ba32f8fd5fd5d7
poorvasha murugavel <poorvashadmp@gmail.com>
2019-08-24 21:19:54 +0530

    UI completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { MyOrderPage } from '../pages/my-order/my-order';
+import { OrderBookedPage } from '../pages/order-booked/order-booked';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    MyOrderPage,
+    OrderBookedPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +23,9 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    MyOrderPage,
+    OrderBookedPage
   ],
   providers: [
     StatusBar,
