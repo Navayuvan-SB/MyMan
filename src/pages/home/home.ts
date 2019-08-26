bf299a1318bdcd2af4abfe34df1b1daa3f14b63d
navayuvan-sb <ns.navayuvan@gmail.com>
2019-08-26 17:19:50 +0530

    Merged Ramya branch

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { PhotographyPage } from '../photography/photography';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,10 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  photography(){
+    this.navCtrl.push(PhotographyPage);
   }
 
 }
